5b85ae5963620a8cb25c0b8d2c94246e9c6ff91e
courticus80 <70612785+courticus80@users.noreply.github.com>
2020-09-02 12:43:39 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PHP Store</title>
+	</head>
+	<body>
+		<?php
+		//using two forward slashes we create comments in php.
+		//Within the php tag, we can insert value into our HTML.
+		echo "<h1>Welcome!</h1>";
+		?>
+	</body>
+</html>
